6c92823ed3dbb5f6ba00d6aa0da46d697330107d
Ramzi9898 <120023341+Ramzi9898@users.noreply.github.com>
2022-12-07 23:30:00 +0100

    Inscription Src

---
+++ VerificationPackage.java
@@ -0,0 +1,14 @@
+import java.sql.SQLException;
+
+public class verif_Pack {
+	public void verif(Universite univ,Etudiant stud)throws SQLException	 {
+	if (univ.getPack() == TypePackage.Standard)
+    {
+         stud.setNbLivreMensuel_Autorise(10);
+    }
+    else if (univ.getPack() == TypePackage.Premium)
+    {
+   	 stud.setNbLivreMensuel_Autorise(10*2);
+    }
+	}
+}
