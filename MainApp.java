3a60dc9cf09c2dcbdfd26c5ff0302c416fda3fdc
Ramzi9898 <120023341+Ramzi9898@users.noreply.github.com>
2022-12-07 23:41:30 +0100

    TP2 / Src

--- MainApp.java
+++ MainApp.java
@@ -1,11 +1,11 @@
 import java.sql.SQLException;
-/*import java.sql.SQLException;
 public class MainApp {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		ViewInscription theView = new ViewInscription();
-		theView.setVisible(true);
+
+
+
 		EtudiantService serv=new EtudiantService();
 		try {
 			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
